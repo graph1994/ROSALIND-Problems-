f9d6cb06b1eceb1dc50b0b2763be818e27f288a1
Kurt Graff <graff1994@gmail.com>
2015-09-21 19:39:35 -0400

    Created Fasta reader to help reduce the problem of having to read from Fasta files.

--- parse_fasta/parse_fasta.py
+++ parse_fasta/parse_fasta.py
@@ -1,1 +1,18 @@
 __author__ = 'graphite'
+
+#Script made to deal with reading Fasta Files.
+#It requires a file name and it reads that file breaking it down at each record.
+# Saves records as Dictonary with Record Id as Key and item as Base pairs.
+
+def parse_fasta(file):
+    data = open(file).readlines()
+    records = {}
+    record_id = 0
+    for line in data:
+
+        if line.startswith('>'):
+            record_id = line[1:].rstrip()
+            records[record_id] = ''
+        else:
+            records[record_id] += line.rstrip()
+    return records
